b29436a52a3ef3b4e953cd40a4f588bb24ee8f88
William Segelström <xwilen22@gmail.com>
2020-01-28 15:20:50 +0100

    Implemented handlebars to the web-application

--- web-application/src/pl/app.js
+++ web-application/src/pl/app.js
@@ -1,11 +1,19 @@
 const express = require("express")
+const handlebars = require("express-handlebars")
 const app = express()
 
 const listenPort = 8080
 
+app.set("views", "src/pl/views")
+
+app.engine("hbs", handlebars({
+    defaultLayout: "main.hbs"
+}))
+
 app.get("/", function(request, response) {
-    response.send("my firts websit :)")
+    response.render("home.hbs")
 })
 app.listen(listenPort, function() {
     console.log(`Listening on port ${listenPort}`)
+    //7dlkajflkeajfölkjfalkejgölkeajglk HEJ
 })
