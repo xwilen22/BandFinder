a05fa82e3a78069ccf8c739e846c8d8bdf41dd59
William Segelström <xwilen22@gmail.com>
2020-01-30 17:50:40 +0100

    Added every screen except edit screens

--- web-application/src/pl/app.js
+++ web-application/src/pl/app.js
@@ -15,6 +15,22 @@ app.engine("hbs", handlebars({
 app.get("/", function(request, response) {
     response.render("home.hbs")
 })
+app.get("/signinup", function(request, response) {
+    response.render("signinup.hbs")
+})
+app.get("/account", function(request, response) {
+    response.render("userdetail.hbs")
+})
+app.get("/band/?", function(request, response) {
+    response.render("banddetail.hbs")
+})
+app.get("/browse", function(request, response) {
+    response.render("browse.hbs")
+})
+app.get("/noband", function(request, response) {
+    response.render("noband.hbs")
+})
+
 app.listen(listenPort, function() {
     console.log(`Listening on port ${listenPort}`)
 })
