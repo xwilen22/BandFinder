b4ddfee090983d09e2e368d4d3a2b0202d186696
William Segelström <xwilen22@gmail.com>
2020-01-31 13:46:54 +0100

    Added routers for each respective resource

--- web-application/src/pl/app.js
+++ web-application/src/pl/app.js
@@ -2,6 +2,10 @@ const express = require("express")
 const handlebars = require("express-handlebars")
 const app = express()
 
+const accountRouter = require("./routers/accountRouter")
+const bandRouter = require("./routers/bandRouter")
+const instrumentRouter = require("./routers/instrumentRouter")
+
 const listenPort = 8080
 
 app.use(express.static(__dirname + "/public"))
@@ -12,6 +16,10 @@ app.engine("hbs", handlebars({
     defaultLayout: "main.hbs"
 }))
 
+app.use("/bands", bandRouter)
+app.use("/account", accountRouter)
+app.use("/instrument", instrumentRouter)
+
 app.get("/", function(request, response) {
     response.render("home.hbs")
 })
@@ -21,22 +29,12 @@ app.get("/signinup", function(request, response) {
 app.get("/account", function(request, response) {
     response.render("userdetail.hbs")
 })
-app.get("/band/?", function(request, response) {
-    response.render("banddetail.hbs")
-})
-app.get("/browse", function(request, response) {
-    response.render("browse.hbs")
-})
 app.get("/noband", function(request, response) {
     response.render("noband.hbs")
 })
-app.get("/account/edit/?", function(request, response) {
-    response.render("edituser.hbs")
-})
-app.get("/band/edit/?", function(request, response) {
-    response.render("manageband.hbs")
-})
+app.get("/admin", function(request, response) {
 
+})
 app.listen(listenPort, function() {
     console.log(`Listening on port ${listenPort}`)
 })
---
+++ web-application/src/pl/routers/accountRouter.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("view/:username", function(request, response) {
+
+})
+router.post("delete/:username", function(request, response) {
+    
+})
+router.post("update/:username", function(request, response) {
+
+})
+router.post("create", function(request, response) {
+
+})
+module.exports = router
