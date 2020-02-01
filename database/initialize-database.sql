04bbd27209163f1810189b85118c3deef4b5dc89
William Segelström <xwilen22@gmail.com>
2020-02-01 12:17:33 +0100

    Added new tables for initialization

--- database/initialize-database.sql
+++ database/initialize-database.sql
@@ -2,19 +2,23 @@ CREATE TABLE IF NOT EXISTS user (
     username VARCHAR(20) PRIMARY KEY,
     password VARCHAR(30) NOT NULL,
     biography TEXT,
-    user_profile_picture BLOB,
-    band_id INT,
-    CONSTRAINT FOREIGN KEY band_id REFERENCES band (band_id)
+    user_profile_picture BLOB
 );
+CREATE TABLE IF NOT EXISTS band_membership(
+    username VARCHAR(20),
+    band_id VARCHAR(30),
+    is_band_leader BOOLEAN,
+    CONSTRAINT FOREIGN KEY username REFERENCES user (username),
+    CONSTRAINT FOREIGN KEY band_id REFERENCES band (band_id)
+)
 CREATE TABLE IF NOT EXISTS band(
     band_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     band_name VARCHAR(30) NOT NULL,
     band_biography TEXT,
-    band_genre_name VARCHAR(20),
+    band_genre VARCHAR(20),
     max_members TINYINT UNSIGNED NOT NULL,
     band_profile_picture BLOB,
-    band_leader_username VARCHAR(20),
-    CONSTRAINT FOREIGN KEY band_leader_username REFERENCES user (username)
+    CONSTRAINT band_genre REFERENCES genre (genre_name)
 );
 CREATE TABLE IF NOT EXISTS instrument (
     instrument_name VARCHAR(20) PRIMARY KEY
