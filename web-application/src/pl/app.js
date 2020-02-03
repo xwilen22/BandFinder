3bd700b154d61f59c7a7b82756b5d1c2f4e924dc
William Segelström <xwilen22@gmail.com>
2020-02-03 13:35:32 +0100

    Usermanager implemented onto accountRouter, body-parser in use on app.js

--- web-application/src/pl/app.js
+++ web-application/src/pl/app.js
@@ -6,11 +6,17 @@ const accountRouter = require("./routers/accountRouter")
 const bandRouter = require("./routers/bandRouter")
 const instrumentRouter = require("./routers/instrumentRouter")
 
+const bodyParser = require("body-parser")
+
 const listenPort = 8080
 
 app.use(express.static(__dirname + "/public"))
 
 app.set("views", "src/pl/views")
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 app.engine("hbs", handlebars({
     defaultLayout: "main.hbs"
--- web-application/src/pl/routers/accountRouter.js
+++ web-application/src/pl/routers/accountRouter.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
+const accountManager = require("./../../bll/accountManager")
+
 //Redirects to account detail or login screen
 router.get("/", function(request, response) {
     let username = 0
@@ -24,6 +26,18 @@ router.post("/update/:username", function(request, response) {
 
 })
 router.post("/create", function(request, response) {
+    const username = request.body.username
+    const password = request.body.password
 
+    accountManager.signUpAccount(username, password, function(error, username) {
+        if(error) {
+            response.send("Get fucked")
+        }
+        else {
+            accountManager.
+            response.render("userdetail.hbs")
+        }
+    })
+    
 })
 module.exports = router
