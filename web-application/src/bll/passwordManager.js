c3a96e57d12eb1ca895c54e16777bb2fc332ab02
William Segelström <xwilen22@gmail.com>
2020-02-03 14:36:58 +0100

    salt rounds are now used

--- web-application/src/bll/passwordManager.js
+++ web-application/src/bll/passwordManager.js
@@ -4,7 +4,7 @@ const HASH_SALT_ROUNDS = 10
 
 module.exports = {
     generatePasswordHash: function(plainPassword, callback) {
-        bcrypt.hash(plainPassword, function(error, hashedPassword){
+        bcrypt.hash(plainPassword, HASH_SALT_ROUNDS, function(error, hashedPassword){
             callback(error, hashedPassword)
         })
     },
