e55b8d4b72781e5ce2ef521bb73d067c1de09f0f
William Segelström <xwilen22@gmail.com>
2020-02-05 15:25:39 +0100

    Fixed syntax errors

--- database/initialize-database.sql
+++ database/initialize-database.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS band_membership(
     username VARCHAR(20),
     band_id VARCHAR(30),
     is_band_leader BOOLEAN,
-    CONSTRAINT FOREIGN KEY username REFERENCES user (username),
-    CONSTRAINT FOREIGN KEY band_id REFERENCES band (band_id)
+    CONSTRAINT FOREIGN KEY (username) REFERENCES user (username),
+    CONSTRAINT FOREIGN KEY (band_id) REFERENCES band (band_id)
 )
 CREATE TABLE IF NOT EXISTS band(
     band_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS band(
     band_genre VARCHAR(20),
     max_members TINYINT UNSIGNED NOT NULL,
     band_profile_picture BLOB,
-    CONSTRAINT FOREIGN KEY band_genre REFERENCES genre (genre_name)
+    CONSTRAINT FOREIGN KEY (band_genre) REFERENCES genre (genre_name)
 );
 CREATE TABLE IF NOT EXISTS instrument (
     instrument_name VARCHAR(20) PRIMARY KEY
@@ -27,10 +27,10 @@ CREATE TABLE IF NOT EXISTS user_proficency (
     username VARCHAR(20),
     instrument_name VARCHAR(20),
     proficency_level TINYINT UNSIGNED NOT NULL,
-    CONSTRAINT FOREIGN KEY username REFERENCES user (username)
+    CONSTRAINT FOREIGN KEY (username) REFERENCES user (username)
 );
 CREATE TABLE IF NOT EXISTS genre (
     genre_name VARCHAR(20),
     parent_genre VARCHAR(20),
-    CONSTRAINT FOREIGN KEY parent_genre REFERENCES genre (genre_name)
+    CONSTRAINT FOREIGN KEY (parent_genre) REFERENCES genre (genre_name)
 );
