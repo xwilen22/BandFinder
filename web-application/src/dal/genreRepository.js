d00c05dbae7f6b4e85cc4afe755e77aa7aa61496
Rasmus Andersparr <andersparr@gmail.com>
2020-02-07 14:35:32 +0100

    added the delete account SQL query, and updated the proficiency repo + the genre repo

--- web-application/src/dal/genreRepository.js
+++ web-application/src/dal/genreRepository.js
@@ -17,16 +17,53 @@ module.exports = {
     createSubGenreOf: function(genreName,parentGenre,callback){
         let query = `INSERT INTO genre (genre_name, parent_genre) VALUES (? , ?)`
         let values = [genreName,parentGenre]
-        db.query(query, [values], function(error){
+        db.query(query, values, function(error){
             callback(error)
         })
     },
 
     deleteSubGenre: function(genreName,parentGenre,callback){
-        let query = `DELETE * FROM genre WHERE (genre_name, parent_genre) = (? , ?)`
+        let query = `DELETE * FROM genre WHERE (genre_name) = (?) AND (parent_genre) = (?)`
         let values = [genreName,parentGenre]
-        db.query(query, [values],function(error){
+        db.query(query, values,function(error){
             callback(error)
+        })
+    },
+
+    getParentGenresByName: function(parentGenre,callback){
+        let query = `SELECT * FROM genre WHERE (genre_name) = (?)`
+        db.query(query,[parentGenre],function(error,parentGenres){
+            if (error){
+                callback(error)
+            }
+            else{
+                callback(genres)
+            }
+        }) 
+    },
+
+    getSubGenresByName: function(genreName,parentGenre,callback){
+        let query = `SELECT * FROM genre WHERE (parent_genre) = (?) AND (genre_name) = (?)`
+        let values = [parentGenre, genreName]
+        db.query(query,values,function(error,subGenres){
+            if(error){
+                callback(error)
+            }
+            else{
+                callback(subGenres)
+            }
+        })
+    },
+
+    getAllGenres: function(callback){
+        let query = `SELECT * FROM genre`
+        db.query(query,function(error,genres){
+            if(error){
+                callback(error)
+            }
+            else{
+                callback(genres)
+            }
         })
     },
 
