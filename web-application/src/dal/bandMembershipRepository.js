0f457a6be1960dd5f959a0b35ba9bdc7692d281c
Rasmus Andersparr <andersparr@gmail.com>
2020-02-07 11:43:27 +0100

    Fixed SQL query syntax, and added the instrument + proficiency  repos

--- web-application/src/dal/bandMembershipRepository.js
+++ web-application/src/dal/bandMembershipRepository.js
@@ -8,25 +8,25 @@ const db = mysql.createConnection({
 
 module.exports = {
     createBandMembership: function(username, bandId, isBandleader,callback){
-        let query = `INSERT INTO band_membership (username,band_id,is_band_leader) VALUES ?`
+        let query = `INSERT INTO band_membership (username,band_id,is_band_leader) VALUES (? , ? , ?)`
         let values = [username,bandId,isBandleader]
-        db.query(query,[values],function(error){
+        db.query(query,values,function(error){
             callback(error)
         })
     },
 
     updateBandMemberToLeader: function(username, bandId, isBandleader, callback){
-        let query = `UPDATE band_membership SET (is_band_leader) VALUES ? WHERE (username,band_id) = ? `
+        let query = `UPDATE band_membership SET (is_band_leader) VALUES (?) WHERE (username,band_id) = (? , ?)`
         let values = [isBandleader,username,bandId]
-        db.query(query, [values], function(error){
+        db.query(query, values, function(error){
             callback(error)
         })
     },
     
     deleteBandMembership: function(username, bandId, callback){
-        let query = `DELETE FROM band_membership WHERE (username, band_id) = ?`
+        let query = `DELETE * FROM band_membership WHERE (username, band_id) = (? , ?)`
         let values = [username, bandId]
-        db.query(query,[values],function(error){
+        db.query(query,values,function(error){
             callback(error)
         })
     }
---
+++ web-application/src/dal/instrumentRepository.js
@@ -0,0 +1,45 @@
+const mysql =  require("mysql")
+const db = mysql.createConnection({
+    host:"database",
+    user:"root",
+    port:"3306",
+    password:"DucTreHouHa",
+    database: "bandFinderDatabase"
+})
+
+module.exports = {
+    createNewInstrument: function(instrument){
+        let query = `INSERT INTO instrument (instrument_name) VAlUES (?)`
+        db.query(query,[instrument],function(error){
+            callback(error)
+        })
+    },
+
+    getAllInstrument: function(){
+        let query = `SELECT * FROM instrument`
+        db.query(query,function(error,instruments){
+            if(error){
+                callback(error)
+            }
+            else{
+                callback(instruments)
+            }
+        })
+    },
+
+    getInstrumentByName: function(instrument){
+        let query = `SELECT * FROM instrument WHERE (instrument_name) = (?)`
+        db.query(query,[instrument],function(error,instrument){
+            if(error){
+                callback(error)
+            }
+            else{
+                callback(instrument)
+            }
+        })
+    },
+
+    deleteInstrumentByName: function(instrument){
+        
+    }
+}
