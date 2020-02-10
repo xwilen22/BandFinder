e3a7bbfa15f112f5cf130614f4346326fdf3ff1c
William Segelström <xwilen22@gmail.com>
2020-02-10 15:27:34 +0100

    Installed awilix package, implementation of main.js progress

---
+++ web-application/src/main.js
@@ -0,0 +1,39 @@
+const awilix = require("awilix")
+
+const proficiencyRepository = require("./dal/proficiencyRepository")
+const accountRepository = require("./dal/accountRepository")
+const accountManager = require("./bll/accountManager")
+const accountRouter = require("./pl/routers/accountRouter")
+
+const passwordManager = require("./bll/passwordManager")
+const accountValidation = require("./bll/accountValidation")
+
+const instrumentRepository = require("./dal/instrumentRepository")
+const instrumentRouter = require("./pl/routers/instrumentRouter")
+
+const genreRepository = require("./dal/genreRepository")
+const bandMembershipRepository = require("./dal/bandMembershipRepository")
+const bandRepository = require("./dal/bandRepository")
+const bandManager = require("./bll/bandManager")
+const bandRouter = require("./pl/routers/bandRouter")
+
+const variousRouter = require("./pl/routers/variousRouter")
+
+const container = awilix.createContainer()
+container.register("accountRepository", awilix.asFunction(accountRepository))
+container.register("accountManager", awilix.asFunction(accountManager))
+container.register("accountRouter", awilix.asFunction(accountRouter))
+
+const theAccountRouter = container.resolve("accountRouter")
+
+container.register("proficiencyRepository", awilix.asFunction(proficiencyRepository))
+
+container.register("passwordManager", awilix.asFunction(passwordManager))
+container.register("accountValidation", awilix.asFunction(accountValidation))
+
+container.register("instrumentRepository", awilix.asFunction(instrumentRepository))
+container.register("genreRepository", awilix.asFunction(genreRepository))
+container.register("bandMembershipRepository", awilix.asFunction(bandMembershipRepository))
+container.register("bandRepository", awilix.asFunction(bandRepository))
+container.register("bandManager", awilix.asFunction(bandManager))
+
