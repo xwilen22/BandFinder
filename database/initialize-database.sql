e6e9d52b542b274598b1d97c1f9aa62c10f2eee3
William Segelström <xwilen22@gmail.com>
2020-02-21 09:53:20 +0100

    Fixed SQL Syntax error on initialization

--- database/initialize-database.sql
+++ database/initialize-database.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS user (
     user_profile_picture BLOB
 );
 CREATE TABLE IF NOT EXISTS genre (
-    genre_name VARCHAR(20) NOT NULL,
+    genre_name VARCHAR(20) PRIMARY KEY,
     parent_genre VARCHAR(20),
     CONSTRAINT subGenreContraint FOREIGN KEY (parent_genre) REFERENCES genre(genre_name)
 );
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS band (
     band_id INT AUTO_INCREMENT NOT NULL,
     band_name VARCHAR(30) NOT NULL,
     band_biography TEXT,
-    band_genre VARCHAR(20),
+    band_genre VARCHAR(20) NOT NULL,
     max_members TINYINT UNSIGNED NOT NULL,
     band_profile_picture BLOB,
     PRIMARY KEY (band_id),
@@ -26,12 +26,12 @@ CREATE TABLE IF NOT EXISTS user_proficiency (
     username VARCHAR(20) NOT NULL,
     instrument_name VARCHAR(20),
     proficiency_level TINYINT UNSIGNED NOT NULL,
-    CONSTRAINT dingus FOREIGN KEY (username) REFERENCES user(username)
+    CONSTRAINT FOREIGN KEY (username) REFERENCES user(username)
 );
 CREATE TABLE IF NOT EXISTS band_membership (
-    username VARCHAR(20),
-    band_id INT,
+    username VARCHAR(20) NOT NULL,
+    band_id INT NOT NULL,
     is_band_leader BOOLEAN,
-    CONSTRAINT fungo FOREIGN KEY (username) REFERENCES user(username),
-    CONSTRAINT jarlplebus FOREIGN KEY (band_id) REFERENCES band(band_id)
+    CONSTRAINT FOREIGN KEY (username) REFERENCES user(username),
+    CONSTRAINT FOREIGN KEY (band_id) REFERENCES band(band_id)
 );
