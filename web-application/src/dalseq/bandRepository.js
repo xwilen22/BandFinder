0d9470734e72fad39c86857c2012325d0166e5d1
William Segelström <xwilen22@gmail.com>
2020-02-26 17:38:42 +0100

    Converted band repo

--- web-application/src/dalseq/bandRepository.js
+++ web-application/src/dalseq/bandRepository.js
@@ -1,23 +1,75 @@
 module.exports = function ({ db }) {
+    const bandModel = db.model("band")
+    
     return {
         getAllBands: function (callback) {
-
+            bandModel.findAll()
+            .then(bands => {
+                callback(undefined, bands)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
 
         getBandById: function (id, callback) {
-
+            bandModel.findAll({
+                where: {
+                    band_id:id
+                }
+            })
+            .then(band => {
+                callback(undefined, band)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
 
         updateBandById: function (id, bandname, bandbio, genre, callback) {
-
+            bandModel.update({
+                band_name:bandname,
+                band_biography:bandbio,
+                band_genre:genre
+            },{
+                where: {
+                    band_id:id
+                }
+            })
+            .then(band => {
+                callback(undefined, band.band_id)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
 
         createBand: function (bandname, bandbio, genre, callback) {
-
+            bandModel.create({
+                band_name:bandname,
+                band_biography:bandbio,
+                band_genre:genre
+            })
+            .then(band => {
+                callback(undefined, band.band_id)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
 
         deleteBandById: function(bandId) {
-
+            bandModel.delete({
+                where: {
+                    band_id:bandId
+                }
+            })
+            .then(() => {
+                callback(undefined, null)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         }
     }
 }
