c5d3ccbf28883b19442f529af8e02837b1736d40
William Segelström <xwilen22@gmail.com>
2020-02-26 17:32:26 +0100

    Rewrote bandmembership repo

--- web-application/src/dalseq/bandMembershipRepository.js
+++ web-application/src/dalseq/bandMembershipRepository.js
@@ -1,15 +1,51 @@
 module.exports = function ({ db }) {
+    const bandMembershipModel = db.model("band_membership")
+
     return {
         createBandMembership: function (username, bandId, isBandleader, callback) {
-
+            bandMembershipModel.create({
+                username,
+                band_id:bandId,
+                is_band_leader:isBandleader
+            })
+            .then(() => {
+                callback(undefined, null)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
 
         updateBandMemberLeaderStatus: function (username, bandId, isBandleader, callback) {
-
+            bandMembershipModel.update({
+                is_band_leader:isBandleader
+            },{
+                where: {
+                    username,
+                    band_id:bandId
+                }
+            })
+            .then(() => {
+                callback(undefined, null)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
 
         deleteBandMembership: function (username, bandId, callback) {
-
+            bandMembershipModel.delete({
+                where: {
+                    username,
+                    band_id:bandId
+                }
+            })
+            .then(() => {
+                callback(undefined, null)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         }
     }
 }
