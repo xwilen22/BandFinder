322041eeb497a38c01485986415249ad420bdec5
William Segelström <xwilen22@gmail.com>
2020-02-26 17:25:00 +0100

    Recreated account repo

--- web-application/src/dalseq/accountRepository.js
+++ web-application/src/dalseq/accountRepository.js
@@ -16,19 +16,64 @@ module.exports = function ({ db }) {
         },
 
         updateUserInfoByUsername: function (username, bio, userPicture, callback) {
-
+            userModel.update({
+                biography: bio, 
+                user_profile_picture: userPicture
+            },{
+                where: {
+                    username
+                }
+            })
+            .then(user => {
+                callback(undefined, user.username)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
 
         updateUserPassword: function (username, password, callback) {
-
+            userModel.update({
+                password
+            },{
+                where: {
+                    username
+                }
+            })
+            .then(user => {
+                callback(undefined, user.username)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
 
         getUserByUsername: function (username, callback) {
-
+            userModel.findAll({
+                where: {
+                    username
+                }
+            })
+            .then(user => {
+                callback(undefined, user)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
 
         deleteUserByUsername: function (username, callback) {
-
+            userModel.delete({
+                where: {
+                    username
+                }
+            })
+            .then(() => {
+                callback(undefined, null)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         }
     }
 }
