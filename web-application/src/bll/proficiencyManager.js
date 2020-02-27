b184e9ca3f9154248f7816464b989d583f38882d
William Segelström <xwilen22@gmail.com>
2020-02-27 13:08:20 +0100

    Work on prof. Manager, added validation for prof.

--- web-application/src/bll/proficiencyManager.js
+++ web-application/src/bll/proficiencyManager.js
@@ -1,5 +1,67 @@
-module.exports = function ({errorGenerator, proficiencyRepository}) { 
+module.exports = function ({errorGenerator, proficiencyRepository, proficiencyValidation}) { 
     return {
-        
+        createProficiency(username, instrumentName, proficiencyLevelNumber, callback) {
+            proficiencyValidation.retrieveUsernameAndInstrumentName(username, instrumentName, function(formatedError, usernameAndInstrumentObject) {
+                if(formatedError) {
+                    callback(formatedError)
+                }
+                else {
+                    const retrievedUsername = usernameAndInstrumentObject.username
+                    const retrievedInstrumentName = usernameAndInstrumentObject.instrumentName
+
+                    proficiencyRepository.createUserProficiency(retrievedUsername, retrievedInstrumentName, function(error) {
+                        if(error) {
+                            callback(errorGenerator.getInternalError(error))
+                        }
+                        else {
+                            callback([])
+                        }
+                    })
+                }
+            })
+        },
+        updateProficiencyLevelForUser(username, instrumentName, newLevelNumber, callback) {
+            proficiencyValidation.retrieveUsernameAndInstrumentName(username, instrumentName, function(formatedError, usernameAndInstrumentObject) {
+                if(formatedError) {
+                    callback(formatedError)
+                }
+                else {
+                    const retrievedUsername = usernameAndInstrumentObject.username
+                    const retrievedInstrumentName = usernameAndInstrumentObject.instrumentName
+
+                    proficiencyRepository.updateUserProficiencyLevel(retrievedUsername, retrievedInstrumentName, newLevelNumber, function(error) {
+                        if(error) {
+                            callback(errorGenerator.getInternalError(error))
+                        }
+                        else {
+                            callback([])
+                        }
+                    })
+                }
+            })
+        },
+        deleteProficiencyForUser(username, instrumentName, callback) {
+            proficiencyValidation.retrieveUsernameAndInstrumentName(username, instrumentName, function(formatedError, usernameAndInstrumentObject) {
+                if(formatedError) {
+                    callback(formatedError)
+                }
+                else {
+                    const retrievedUsername = usernameAndInstrumentObject.username
+                    const retrievedInstrumentName = usernameAndInstrumentObject.instrumentName
+
+                    proficiencyRepository.deleteUserProficiency(retrievedUsername, retrievedInstrumentName, function(error) {
+                        if(error) {
+                            callback(errorGenerator.getInternalError(error))
+                        }
+                        else {
+                            callback([])
+                        }
+                    })
+                }
+            })
+        },
+        getAllProficienciesForUser(username, callback) {
+
+        }
     }
 }
---
+++ web-application/src/bll/proficiencyValidation.js
@@ -0,0 +1,29 @@
+module.exports = function ({accountManager, instrumentManager}) {
+    return {
+        retrieveUsernameAndInstrumentName(username, instrumentName) {
+            accountManager.getAccountByUsername(username, function(error, userObject) {
+                if(error) {
+                    callback(errorGenerator.getInternalError(error))
+                }
+                else if(userObject == null) {
+                    callback(errorGenerator.getClientError(["User does not exist"]))
+                }
+                else {
+                    instrumentManager.getInstrumentByName(instrumentName, function(error, instrumentObject) {
+                        if (error) {
+                            callback(errorGenerator.getInternalError(error))
+                        }
+                        else if (instrument == null) {
+                            callback(errorGenerator.getClientError(["Instrument does not exist"]))
+                        }
+                        else {
+                            const retrievedUsername = userObject.username
+                            const retrievedInstrumentName = instrumentObject.instrument_name
+                            callback([], {username: retrievedUsername, instrumentName: retrievedInstrumentName})
+                        }
+                    })
+                }
+            })
+        }
+    }
+}
