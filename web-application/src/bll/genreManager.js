150857def195d4cff984d16e357679b584c944bc
William Segelström <xwilen22@gmail.com>
2020-02-27 11:18:53 +0100

    Added functionality to genreManager

--- web-application/src/bll/genreManager.js
+++ web-application/src/bll/genreManager.js
@@ -1,5 +1,50 @@
 module.exports = function ({errorGenerator, genreRepository}) { 
     return {
-        
+        createGenre: function(genreName, callback) {
+            genreRepository.createGenre(genreName, function(error) {
+                if(error) {
+                    callback(errorGenerator.getInternalError(error))
+                } 
+                else {
+                    callback([])
+                }
+            })
+        },
+        createSubGenreOf: function(parentGenreName, subGenreName, callback) {
+            genreRepository.createSubGenreOf(parentGenreName, subGenreName, function(error) {
+                if(error) {
+                    callback(errorGenerator.getInternalError(error))
+                } 
+                else {
+                    callback([])
+                }
+            })
+        },
+        deleteGenreByName: function(genreName, callback) {
+            genreRepository.deleteGenreByName(genreName, function(error) {
+                if(error) {
+                    callback(errorGenerator.getInternalError(error))
+                } 
+                else {
+                    callback([])
+                }
+            })
+        },
+        getAllGenres: function(callback) {
+            genreRepository.getAllGenres(function(error) {
+                if(error) {
+                    callback(errorGenerator.getInternalError(error))
+                } 
+                else {
+                    callback([])
+                }
+            })
+        },
+        getAllSubGenresOf: function(parentGenreName, callback) {
+
+        },
+        getGenreByName: function(genreName, callback) {
+
+        }
     }
 }
