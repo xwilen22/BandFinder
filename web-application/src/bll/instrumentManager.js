d873e026fc012d6599dfb9bd5028e52d2830e0a4
William Segelström <xwilen22@gmail.com>
2020-02-27 12:15:43 +0100

    Added instrument manager

--- web-application/src/bll/instrumentManager.js
+++ web-application/src/bll/instrumentManager.js
@@ -1,5 +1,44 @@
 module.exports = function ({errorGenerator, instrumentRepository}) { 
     return {
-        
+        createInstrument: function(instrumentName, callback) {
+            instrumentRepository.createInstrument(instrumentName, function(error) {
+                if(error) {
+                    callback(errorGenerator.getInternalError(error))
+                }
+                else {
+                    callback([])
+                }
+            })
+        },
+        deleteInstrument: function(instrumentName, callback) {
+            instrumentRepository.deleteInstrumentByName(instrumentName, function(error) {
+                if(error) {
+                    callback(errorGenerator.getInternalError(error))
+                }
+                else {
+                    callback([])
+                }
+            })
+        },
+        getInstrumentByName: function(instrumentName, callback) {
+            instrumentRepository.getInstrumentByName(instrumentName, function(error, instrument) {
+                if(error) {
+                    callback(errorGenerator.getInternalError(error))
+                }
+                else {
+                    callback([], instrument)
+                }
+            })
+        },
+        getAllInstruments: function(callback) {
+            instrumentRepository.getAllInstruments(function(error, instruments) {
+                if(error) {
+                    callback(errorGenerator.getInternalError(error))
+                }
+                else {
+                    callback([], instruments)
+                }
+            })
+        }
     }
 }
