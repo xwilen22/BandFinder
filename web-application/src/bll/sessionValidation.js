2c719ce122038f4941d923629084fa5d920265a2
Rasmus Andersparr <andersparr@gmail.com>
2020-03-05 14:38:48 +0100

    band leader validation might work now

--- web-application/src/bll/sessionValidation.js
+++ web-application/src/bll/sessionValidation.js
@@ -4,15 +4,29 @@ module.exports = function({}) {
             return accountName == sessionAccountName
         },
 
-        validateSsCurrentUserBandLeader: function(bandmembers, sessionAccountName){
-            for(const member in bandmembers){
+        validateCurrentUserBandLeader: function(bandmembers, sessionAccountName){
+            console.log("DAS MEMBERS: ",bandmembers)
+            bandmembers.forEach(member => {
+                console.log("does it get here", sessionAccountName, member)
                 if(member.username == sessionAccountName){
                     return member.is_band_leader
                 }
                 else{
                     return false
                 }
+            })
+            for(let i = 0; i < bandmembers.length; i++) {
+                console.log("JAVASCRIPT IS A TERRIBLE LANGUAGE", bandmembers[i])
             }
+            /*for(const member of bandmembers){
+                console.log("does it get here",sessionAccountName)
+                if(member.username == sessionAccountName){
+                    return member.is_band_leader
+                }
+                else{
+                    return false
+                }
+            }*/
         }
     }
 }
