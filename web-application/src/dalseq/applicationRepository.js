9e0a5e9d403a783b9603859783af2b0f392f2458
William Segelström <xwilen22@gmail.com>
2020-03-06 13:05:19 +0100

    Implemented dalseq feature for applications

--- web-application/src/dalseq/applicationRepository.js
+++ web-application/src/dalseq/applicationRepository.js
@@ -1,16 +1,60 @@
 module.exports = function ({ db }) {
+    const applicationModel = db.model("band_application")
+
     return {
         createApplication: function(forBandId, byUsername, callback) {
-
+            applicationModel.create({
+                band_id: forBandId,
+                username: byUsername
+            })
+            .then(() => {
+                callback(undefined, null)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
         getApplicationsByBandId: function(bandId, callback) {
-
+            applicationModel.findAll({
+                where: {
+                    band_id:bandId
+                },
+                raw: true
+            })
+            .then(bandApplications => {
+                callback(undefined, bandApplications)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
         getApplicationsByUsername: function(username, callback) {
-
+            applicationModel.findAll({
+                where: {
+                    username
+                },
+                raw: true
+            })
+            .then(bandApplications => {
+                callback(undefined, bandApplications)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         },
         deleteApplication: function(bandId, username, callback) {
-            
+            applicationModel.destroy({
+                where: {
+                    username,
+                    band_id:bandId
+                }
+            })
+            .then(() => {
+                callback(undefined, null)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
         }
     }
 }
