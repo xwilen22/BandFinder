62031e063aca5f8b645e7284016f897b3157c970
William Segelström <xwilen22@gmail.com>
2020-03-06 13:39:07 +0100

    Implemented MySQL functions for applications

--- web-application/src/dal/applicationRepository.js
+++ web-application/src/dal/applicationRepository.js
@@ -1,16 +1,40 @@
 module.exports = function ({ db }) {
     return {
         createApplication: function(forBandId, byUsername, callback) {
+            const query = `INSERT INTO band_application (band_id, username) 
+                           VALUES (?, ?)`
+            const values = [forBandId, byUsername]
 
+            db.query(query, values, function(error) {
+                callback(error)
+            })
         },
         getApplicationsByBandId: function(bandId, callback) {
+            const query = `SELECT * FROM band_application
+                           WHERE band_id = ?`
+            const values = [bandId]
 
+            db.query(query, values, function(error, bandApplications) {
+                callback(error, bandApplications)
+            })
         },
         getApplicationsByUsername: function(username, callback) {
+            const query = `SELECT * FROM band_application
+                           WHERE username = ?`
+            const values = [username]
 
+            db.query(query, values, function(error, bandApplications) {
+                callback(error, bandApplications)
+            })
         },
         deleteApplication: function(bandId, username, callback) {
-            
+            const query = `DELETE FROM band_application 
+                           WHERE band_id = ? AND username = ?`
+            const values = [bandId, username]
+
+            db.query(query, values, function(error) {
+                callback(error)
+            })
         }
     }
 }
