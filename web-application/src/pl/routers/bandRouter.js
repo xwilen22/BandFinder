b7650137639ded88acfe556c7bf556060af47050
William Segelström <xwilen22@gmail.com>
2020-03-09 15:27:57 +0100

    Styling on band detail, applications are now retrieved if bandleader, possible to delete applications via UI

--- web-application/src/pl/routers/bandRouter.js
+++ web-application/src/pl/routers/bandRouter.js
@@ -6,7 +6,6 @@ module.exports = function ({bandManager, bandMembershipManager, genreManager, se
     //Get all
     router.get("/", function (request, response, next) {
         const sessionUsername = request.session.loggedInUsername
-
         bandManager.getAllBands(function(error, bands) {
             if(error) {
                 next(error)
@@ -53,18 +52,28 @@ module.exports = function ({bandManager, bandMembershipManager, genreManager, se
                         next(bandError)
                     }
                     else {
-                        const currentUsername = request.session.loggedInUsername
-                        const bandObject = band
-                        const isCurrentUserBandLeader = sessionValidation.validateCurrentUserBandLeader(bandMembers,currentUsername)
-                        const model = {
-                            bandMembers,
-                            bandId: bandObject.id,
-                            bandname: bandObject.band_name,
-                            biography: bandObject.band_biography,
-                            profilePicture: bandObject.band_profile_picture,
-                            isCurrentUserBandLeader
-                        }
-                        response.render("banddetail.hbs", model)
+                        applicationManager.getApplicationsByBandId(bandId, function (applicationError, bandApplications) {
+                            if (applicationError) {
+                                next(applicationError)
+                            }
+                            else {
+                                const currentUsername = request.session.loggedInUsername
+                                const bandObject = band
+                                const isCurrentUserBandLeader = sessionValidation.validateCurrentUserBandLeader(bandMembers, currentUsername)
+                                
+                                const model = {
+                                    bandMembers,
+                                    bandId: bandObject.id,
+                                    bandname: bandObject.band_name,
+                                    biography: bandObject.band_biography,
+                                    profilePicture: bandObject.band_profile_picture,
+                                    bandApplications,
+                                    isCurrentUserBandLeader
+                                }
+
+                                response.render("banddetail.hbs", model)
+                            }                                                                                                       
+                        })
                     }
                 })
             }
@@ -77,7 +86,7 @@ module.exports = function ({bandManager, bandMembershipManager, genreManager, se
             if(bandMembershipError){
                 next(bandMembershipError)
             }
-            else{
+            else {
                 const model = {
                     bandMemberships
                 }
@@ -122,18 +131,20 @@ module.exports = function ({bandManager, bandMembershipManager, genreManager, se
             }
         })
     })
+
     router.post("/update/:bandId", function (request, response, next) {
         const bandId = request.params.bandId
         const bandname = request.body.bandNameText
         const bio = request.body.bioText
         const genre = request.body.genre
+        
         bandMembershipManager.getBandMembershipByBandId(bandId, function(membershipError, bandMembers){
             if(membershipError){
                 response.send(membershipError)
             }
             else {
                 const validated = sessionValidation.validateCurrentUserBandLeader(bandMembers, request.session.loggedInUsername)
-                if(validated==true){
+                if(validated == true){
                     bandManager.updateBandById(bandId, bio, bandname, genre, function(bandError, bandId){
                         if(bandError){
                             response.send(bandError)
@@ -166,6 +177,7 @@ module.exports = function ({bandManager, bandMembershipManager, genreManager, se
     })
 
     router.post("/create", function (request, response) {
+        
         const bandname = request.body.bandNameText
         const username = request.session.loggedInUsername
         const bio = request.body.bioText
