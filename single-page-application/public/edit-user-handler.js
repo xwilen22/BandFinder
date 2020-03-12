835399c58530ded4730a1edf2f4b7d03c2506e0c
William Segelström <xwilen22@gmail.com>
2020-03-12 15:59:41 +0100

    Added generic update function for updates

--- single-page-application/public/edit-user-handler.js
+++ single-page-application/public/edit-user-handler.js
@@ -11,7 +11,7 @@ function displayEditPageForUser(parentElement, username) {
 
     fetchResource(`account/${username}`, function(error, accountInformationObject) {
         if(error) {
-
+            console.log("Bolloxed", error)
         }
         else {
             usernameHeader.innerText = accountInformationObject.username
@@ -19,7 +19,14 @@ function displayEditPageForUser(parentElement, username) {
 
             biographyEditForm.addEventListener("submit", function(event) {
                 event.preventDefault()
-
+                editBiographyForUsername(accountInformationObject.username, biographyTextarea.value, function(error) {
+                    if(error) {
+                        console.error("no", error)
+                    }
+                    else {
+                        console.log("ye")
+                    }
+                })
             })
         }
     })
@@ -27,7 +34,7 @@ function displayEditPageForUser(parentElement, username) {
     instrumentsSelect.innerHTML = ""
     fetchResource("instruments" ,function(error, instruments) {
         if(error) {
-
+            console.log("Bolloxed", error)
         }
         else {
             for (instrument of instruments) {
@@ -51,15 +58,36 @@ function displayEditPageForUser(parentElement, username) {
                 const updateForm = proficiencyListItem.getElementsByTagName("form")[0]
                 updateForm.getElementsByTagName("input")[0].value = proficiency.proficiency_level
                 
+                const instrumentName = proficiency.instrument_name
+
                 //When user wants to update prof level
                 updateForm.addEventListener("submit", function(event) {
+                    event.preventDefault()
+                    const proficiencyLevel = updateForm.getElementsByTagName("input")[0].value
 
+                    updateResourceAuth(`proficiencies/${username}/${instrumentName}`, {skillLevel:proficiencyLevel}, function(error) {
+                        if(error) {
+                            console.log("bad", error)
+                        }
+                        else {
+                            console.log("Good")
+                        }
+                    })
+                    /*editProficiencyLevelForUsernameAndInstrument(username, proficiency.instrument_name, proficiencyLevel, function(error) {
+                        if(error) {
+                            console.log("Error", error)
+                        }
+                        else {
+                            console.log("Nice")
+                        }
+                    })*/
                 })
 
                 const deleteForm = proficiencyListItem.getElementsByTagName("form")[1]
                 //When user wants to delete prof
                 deleteForm.addEventListener("submit", function(event) {
-
+                    event.preventDefault()
+                    updateResourceAuth("", {skillLevel:1, kuken:"hej"})
                 })
 
                 proficienciesUnorderedList.appendChild(proficiencyListItem)
@@ -89,4 +117,94 @@ function fetchResource(resourceUri, callback) {
         console.log("FAILED", error)
         callback(error)
     })
+}
+function updateResourceAuth(resourceUri, keyValueBodyObject, callback) {
+    let body = ""
+    for(key in keyValueBodyObject) {
+        if(body.length > 0) {
+            body += "&"
+        }
+        body += `${key}=${keyValueBodyObject[key]}`
+    }
+
+    fetch(
+        `${currentDomain}/api/${resourceUri}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "authorization": `Bearer ${localStorage.accessToken}`
+            },
+            body
+        }
+    )
+    .then(response => {
+        if(response.status == 204) {
+            callback(undefined)
+        }
+        else {
+            console.log("Error ", response)
+            callback(["Unexpected result"])
+        }
+    })
+    .catch(error => {
+        console.log("Error ", error)
+        callback(error)
+    })
+}
+function editBiographyForUsername(username, biographyText, callback) {
+    fetch(
+        `${currentDomain}/api/account/${username}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "authorization": `Bearer ${localStorage.accessToken}`
+            },
+            body: `biography=${biographyText}`
+        }
+    )
+    .then(response => {
+        if(response.status == 204) {
+            callback(undefined)
+        }
+        else {
+            console.log("Error ", response)
+            callback(["Unexpected result"])
+        }
+    })
+    .catch(error => {
+        console.log("Error ", error)
+        callback(error)
+    })
+}
+function editProficiencyLevelForUsernameAndInstrument(username, instrumentName, proficiencyLevel, callback) {
+    fetch(
+        `${currentDomain}/api/proficiencies/${username}/${instrumentName}`,
+        {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "authorization": `Bearer ${localStorage.accessToken}`
+            },
+            body: `skillLevel=${proficiencyLevel}`
+        }
+    )
+    .then(response => {
+        if(response.status == 204) {
+            callback(undefined)
+        }
+        else {
+            console.log("Error ", response)
+            callback(["Unexpected result"])
+        }
+    })
+    .catch(error => {
+        console.log("Error ", error)
+        callback(error)
+    })
+}
+function deleteProficiencyForUsernameAndInstrument(username, instrumentName, callback) {
+
+}
+function addProficiencyForUsername(username, instrumentName, proficiencyLevel, callback) {
+
 }
