b330587ca1497f065380b3967f9165dc9e370b84
William Segelström <xwilen22@gmail.com>
2020-03-13 14:15:38 +0100

    Changed function to generic fetcher for getting users

--- single-page-application/public/user-page-detail-handler.js
+++ single-page-application/public/user-page-detail-handler.js
@@ -5,34 +5,14 @@ function displayUserDetailPageForUsername(parentElement, username) {
     
     editUserAnchor.hidden = (decodeURIComponent(username) != localStorage.username)
 
-    getUserInformation(username, function(error, accountInformationObject) {
+    fetchResource(`account/${username}`, function(error, accountInformationObject) {
         if(error) {
-            console.log("ERROR", error)
+
         }
         else {
             usernameHeader.innerText = accountInformationObject.username
             bioparagraphElement.innerText = accountInformationObject.biography
             editUserAnchor.href = `/account/edit/${accountInformationObject.username}`
         }
-    })
-}
-function getUserInformation(username, callback) {
-    let errorResponseCode = undefined
-    
-    fetch(
-        `${currentDomain}/api/account/${username}`
-    ).then(response => {
-        if(response.ok) {
-            return response.json()
-        }
-        else {
-            errorResponseCode = response.status
-        }
-    })
-    .then(accountInformation => {
-        callback(errorResponseCode, accountInformation)
-    })
-    .catch(error => {
-        callback(error)
     })
 }
