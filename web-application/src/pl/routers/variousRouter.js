689fcc7714df3a9583e38874f79c6cd603d09885
William Segelström <xwilen22@gmail.com>
2020-03-19 17:47:36 +0100

    Home screen gets all bands, no more scrollbar on layout

--- web-application/src/pl/routers/variousRouter.js
+++ web-application/src/pl/routers/variousRouter.js
@@ -1,10 +1,20 @@
 const Express = require("express")
 
-module.exports = function ({}) {
+module.exports = function ({ bandManager }) {
     const router = Express.Router()
 
-    router.get("/", function (request, response) {
-        response.render("home.hbs")
+    router.get("/", function (request, response, next) {
+        bandManager.getAllBands(function(error, bands) {
+            if(error) {
+                next(error)
+            }
+            else {
+                const model = {
+                    bands
+                }
+                response.render("home.hbs", model)
+            }
+        })
     })
 
     return router
